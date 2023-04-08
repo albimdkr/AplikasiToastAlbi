ed2022189eaf4c1c81a48606cb353a1687b1aa36
albimdkr <albimudakar04@gmail.com>
2023-04-08 20:14:19 +0700

    add syntak toast and increment number when click button count

--- app/src/main/java/com/example/aplikasitoastalbi/MainActivity.java
+++ app/src/main/java/com/example/aplikasitoastalbi/MainActivity.java
@@ -4,21 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    int angka=0;
+    TextView angkakounter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        angkakounter=findViewById(R.id.txtCount);
     }
 
     public void btnToastClick(View view) {
-        Toast.makeText(this, "Ini aplikasi Toast", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Hallo, Ini Adalah Aplikasi Toast", Toast.LENGTH_SHORT).show();
     }
 
-
     public void btnCountClick(View view) {
+        angka=angka+1;
+        angkakounter.setText(Integer.toString(angka));
     }
 }
